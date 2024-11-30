3ec4f93c3d752010f6bc9aab8e89b1d45996334f
Patrick Niebrzydowski <patrick.niebrzydowski@gmail.com>
2024-11-30 09:45:03 +0100

    add Vue Test Utils & GameBoard component

--- src/utils/calculateScore.test.ts
+++
@@ -1,7 +0,0 @@
-import { calculateScore } from './calculateScore'
-
-describe('score calculation', () => {
-  test('should calculate a score', () => {
-    expect(calculateScore(2, 3)).toEqual(5)
-  })
-})
--- src/utils/calculateScore.ts
+++
@@ -1,1 +0,0 @@
-export const calculateScore = (a: number, b: number): number => a + b
